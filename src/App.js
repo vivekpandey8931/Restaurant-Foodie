95571c908c17f980a27d9d9e89886588ebe6d223
vivek <vivekpandey8931@gmail.com>
2020-05-08 16:30:50 +0530

    single page application part1

--- src/App.js
+++ src/App.js
@@ -1,27 +1,19 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import {Navbar,NavbarBrand} from 'reactstrap';
-import Menu from './components/MenuComponent';
+import Main from './components/MainComponent';
+import {BrowserRouter} from 'react-router-dom';
 import './App.css';
-import {DISHES} from './shared/dishes' ;
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state={
-      dishes: DISHES
-    };
-  }
+
+
   render() {
     return (
+      <BrowserRouter>
       <div >
-      <Navbar dark color="primary">
-      <div className="container">
-      <NavbarBrand href="/">Ristorante</NavbarBrand>
+
+      <Main />
       </div>
-      </Navbar>
-      <Menu dishes={this.state.dishes} />
-      </div>
+      </BrowserRouter>
     );
   }
 }
